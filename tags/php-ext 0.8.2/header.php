f64e44d464ab85d26af098f8a00fd6e3e298ccc3
sergeiw <sergeiw@e0da71ca-fc45-0410-af39-5f59eeb7a8d0>
2008-04-17 21:42:59 +0000

    Tagging the 0.8.2 release of the php-ext project

---
+++ tags/php-ext 0.8.2/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>The PHP-Ext Open Source Project <?php echo $PAGE_TITLE; ?></title>
+    <base href="<?php echo $baseUrl; ?>/" />
+    
+    <link rel="stylesheet" type="text/css" href="site.css" />
+    
+    
+<?php
+if (isset($customHeaders))
+	echo $customHeaders;
+?>
+
+</head>
+<body>
+
+<div id="header">
+
+<h1><img src="images/php-ext2.png" alt="PHP-Ext Logo" title="PHP-Ext" width="110" height="34" /></h1>
+<div>
+<div id="legend">The Web 2.0 PHP Widget Library using Ext JS</div>
+<ul id="menu">
+	<li><a href=".">Home</a></li>
+	<li><a href="?id=docs">Docs</a></li>
+	<li><a href="examples">Samples</a></li>
+	<li><a href="blog">Blog</a></li>
+	<li><a href="?id=download">Download</a></li>
+</ul>
+</div>
+ 
+</div>
+
+<div class="content">
+<?php
+require_once 'leftbar.php';
+?>
+	<div id="centercolumn">
