03682c4a0b92a1dfa3e6c43e855e28064e962b89
sergeiw <sergeiw@e0da71ca-fc45-0410-af39-5f59eeb7a8d0>
2008-08-03 18:37:18 +0000

    Tagging a 0.8.3 preview for the community download

---
+++ tags/php-ext 0.8.3 preview/intro.php
@@ -0,0 +1,39 @@
+    <h2 style="margin-bottom:5px;">The PHP-Ext Open Source Project</h2>
+	<p class="justified">PHP-Ext is an open source widget library written for PHP 4 and 5 to empower the UI Layer.<br>
+	It is based on <a href="http://extjs.com">Ext JS</a> javascript widgets which provide a standard 
+	and powerful API to build Rich Internet Applications.  It basically works as a convenient wrapper
+	for the Ext JS Javascript Objects.
+	</p>
+	<p class="justified">Among other features, PHP-Ext provides useful and common controls to create forms, rich comboboxes, powerful grids and menus.
+	It also promotes the use of JSON and XML client/server communication to populate forms and grids using Ajax.
+	Additionally it has a Javascript helper to ease the javascript code generation and use.
+	</p>	
+	<p class="justified">This project still in beta so keep comming for the final release.  Take a look at the samples.
+	</p>
+	<p>
+	Feel free to download the src and samples to try it out and don't forget to write back your comments and suggestions.
+	</p>
+	
+	<h3 style="margin-bottom:5px;">Latest News</h3>
+	<p><a href="http://php-ext.quimera-solutions.com/?id=download">0.8.2 Released - Download</a></p>
+		
+	<hr>
+	<table border=0 style="background-color: #fff; padding: 5px;" cellspacing=0>
+  <tr><td>
+  <img src="http://groups.google.com/groups/img/3nb/groups_bar.gif"
+         height=26 width=132 alt="Google Groups">
+  </td></tr>
+  <tr><td style="padding-left: 5px">
+  <b>Subscribe to PHP-Ext</b>
+  </td></tr>
+  <form action="http://groups.google.com/group/php-ext/boxsubscribe">
+  <tr><td style="padding-left: 5px;">
+  Email: <input type=text name=email>
+  <input type=submit name="sub" value="Subscribe">
+  </td></tr>
+</form>
+<tr><td align=right>
+  <a href="http://groups.google.com/group/php-ext">Visit this group</a>
+</td></tr>
+</table>
+	
